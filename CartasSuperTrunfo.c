c6061b877aeb3a024d04c37c35248816f6fbcd52
Kethelly-beep <kethellybeatriz8@gmail.com>
2025-02-01 01:22:39 +0000

    corrigindo erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -33,22 +33,23 @@ printf("Informe as caraterísticas da cidade\n");
     scanf("%s", &cod);
     printf("Código: %s\n", cod);
 
-    
+    printf("Informe as caraterísticas da cidade\n");
     printf("Informe a população da cidade:");
     scanf("%d", &populacao);
     printf("População: %d\n", populacao);
 
-    
+    printf("Informe as caraterísticas da cidade\n");
     printf("Informe a quantidade de pontos turísticos:");
     scanf("%d", &pont_turisticos);
     printf("Pontos Turísticos: %d\n", pont_turisticos);
 
-    
+printf("Informe as caraterísticas da cidade\n");
+    printf("Informe as caraterísticas da cidade\n");
     printf("Informe a área:");
     scanf("%f", &area);
     printf("Área: %f\n", area);
 
-    
+    printf("Informe as caraterísticas da cidade\n");
     printf("Informe o PIB:");
     scanf("%f", &pib);
     printf("PIB: %s\n", pib);
